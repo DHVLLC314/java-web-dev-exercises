8fb133229adace3157e0a1a36e51268f2aa569b0
Dream Higher Visions <dreamhighervisions@Dreams-MacBook-Air.local>
2019-11-07 19:36:40 -0600

    finished studio exercise

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,21 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input;
+        input = new Scanner(System.in);
+        double pi = 3.14;
+        double radius;
+        double area;
+
+        System.out.println("Enter a radius: ");
+        radius = input.nextDouble();
+        input.close();
+
+        area = Circle.getArea(radius);
+        System.out.println("The area of a circle of " + radius + " is: " + area);
+
+    }
+}
